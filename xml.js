784e010ad77122cdd327225b04faf9fc4a7d7c06
Adam Biggs <adam.biggs@lightmaker.com>
2016-10-28 13:55:23 -0700

    Make element/attribute sorting optional

--- xml.js
+++ xml.js
@@ -483,11 +483,12 @@ var decode_entities = exports.decodeEntities = function decode_entities(text) {
 	return text;
 };
 
-var compose_xml = exports.stringify = function compose_xml(node, name, indent, indent_string, eol) {
+var compose_xml = exports.stringify = function compose_xml(node, name, indent, indent_string, eol, sort) {
 	// Compose node into XML including attributes
 	// Recurse for child nodes
 	if (typeof(indent_string) == 'undefined') indent_string = "\t";
 	if (typeof(eol) == 'undefined') eol = "\n";
+	if (typeof(sort) == 'undefined') sort = true;
 	var xml = "";
 	
 	// If this is the root node, set the indent to 0
@@ -519,9 +520,10 @@ var compose_xml = exports.stringify = function compose_xml(node, name, indent, i
 			
 			if (node["_Attribs"]) {
 				has_attribs = 1;
-				var sorted_keys = hash_keys_to_array(node["_Attribs"]).sort();
-				for (var idx = 0, len = sorted_keys.length; idx < len; idx++) {
-					var key = sorted_keys[idx];
+				var keys = hash_keys_to_array(node["_Attribs"]);
+				if (sort) keys = keys.sort();
+				for (var idx = 0, len = keys.length; idx < len; idx++) {
+					var key = keys[idx];
 					xml += " " + key + "=\"" + encode_attrib_entities(node["_Attribs"][key]) + "\"";
 				}
 			} // has attribs
@@ -536,10 +538,11 @@ var compose_xml = exports.stringify = function compose_xml(node, name, indent, i
 				} // just text
 				else {
 					xml += eol;
-					
-					var sorted_keys = hash_keys_to_array(node).sort();
-					for (var idx = 0, len = sorted_keys.length; idx < len; idx++) {
-						var key = sorted_keys[idx];					
+
+					var keys = hash_keys_to_array(node);
+					if (sort) keys = keys.sort();
+					for (var idx = 0, len = keys.length; idx < len; idx++) {
+						var key = keys[idx];
 						if ((key != "_Attribs") && key.match(re_valid_tag_name)) {
 							// recurse for node, with incremented indent value
 							xml += compose_xml( node[key], key, indent + 1, indent_string, eol );
